a8bff846200ec49b20bc579c392d798a1b3b5d64
Eric Tran <eric@Erics-MacBook-Pro.local>
2019-10-09 07:54:10 -0400

    make it better

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/signup",(req,res)=>{
     res.render("signup")
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Welcome to BednBreakfast Assignment 1 on PORT: ${PORT}`)
 });
